a143754f2bfc32fbb3fb0b291e3365adc52e37df
pop1238pop <Hattakorn.1238@gmail.com>
2022-08-29 10:59:35 +0700

    LAB06

--- utility.js
+++ utility.js
@@ -1,12 +1,13 @@
 //utility.js
 
 const request = require("request-promise");
-const EXTERNAL_API=""; // put url
-const accessToken=""; // put access token
+const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzQyODAxLCJleHAiOjE2NjE3NDY0MDEsIm5iZiI6MTY2MTc0MjgwMSwianRpIjoiTDRTV1pVTVNQb3ZRaHNQMyIsInN1YiI6ODcsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.Xq5K1jpMeMJnA8mZu30I_83t_sd1x42mMnqLtnJl5bs"; // put access token
 const student = {
-  name: '', // replace with your full name.
-  age: 19, // put your age.
-  gender: '', // replace with your gender
+  id_student:63367230,
+  name: 'Hattakorn', // replace with your full name.
+  age: 20, // put your age.
+  gender: 'Male', // replace with your gender
   department: 'CPE'
 };
 
@@ -14,14 +15,10 @@ const student = {
 exports.findStudentbyId = function (student_id, cb) {
   //-- call external api 
           request({
-            method: "POST",
-            uri: EXTERNAL_API,
+            method: "GET",
+            uri: EXTERNAL_API+student_id,
             headers: {
-                Authorization: `Bearer ${accessToken}`
-            },
-            formData: {
-                message: `HTTP Request :${data.student_id} `,
-                student_id: student_id
+                Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYxNzQyODAxLCJleHAiOjE2NjE3NDY0MDEsIm5iZiI6MTY2MTc0MjgwMSwianRpIjoiTDRTV1pVTVNQb3ZRaHNQMyIsInN1YiI6ODcsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.Xq5K1jpMeMJnA8mZu30I_83t_sd1x42mMnqLtnJl5bs`
             }
         }).then((response) => {
             console.log('Sent');
@@ -38,9 +35,4 @@ exports.findStudentbyId = function (student_id, cb) {
                 "Error": err.message
             });
         });
-}
-
-exports.fakeStudentbyInfo = function (student_id, cb) {
-
-  cb(student);
 }
